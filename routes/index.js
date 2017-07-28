1d6c108eca4c8ad126e410c14e9e1a5c61a22ce3
zhangweiheng <764816538@qq.com>
2017-07-29 01:54:06 +0800

    样式编写及页面拆分

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 router.get('/',function(req,res){
-    res.render('index',{tilte:'首页'})
+    res.render('index',{title:'首页'})
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let path = require('path');
+let app = express();
+app.set('view engine','html');
+app.set('views',path.resolve('views'));
+app.engine('html',require('ejs').__express);
+app.use(express.static(path.resolve('node_modules')));
 let index = require('./routes/index');
 let user = require('./routes/user');
 let article = require('./routes/article');
-let app = express();
-app.set('views engine','html');
-app.set('views',path.resolve('views'));
-app.use(express.static(path.resolve('node_modules')));
-app.engine('html',require('ejs').__express);
 app.use('/', index);
 app.use('/user', user);
 app.use('/article', article);
