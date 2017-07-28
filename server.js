9c1dd980bf938eaf7c6fd4434cafda37e1692e96
zhangweiheng <764816538@qq.com>
2017-07-29 01:20:49 +0800

    渲染模板

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 let express = require('express');
+let path = require('path');
 let index = require('./routes/index');
 let user = require('./routes/user');
 let article = require('./routes/article');
 let app = express();
+app.set('views engine','html');
+app.set('views',path.resolve('views'));
+app.use(express.static(path.resolve('node_modules')));
+app.engine('html',require('ejs').__express);
 app.use('/', index);
 app.use('/user', user);
 app.use('/article', article);
+
 app.listen(8080);
